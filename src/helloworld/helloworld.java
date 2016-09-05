ee774d6dda4f895fc74ff3a18156e5f20d934764
bit-user <orochi13@naver.com>
2016-09-05 17:48:18 +0900

    1st

---
+++ src/helloworld/helloworld.java
@@ -0,0 +1,12 @@
+package helloworld;
+/*
+ 
+ */
+public class helloworld {
+	public static void main(String[] args) {
+		//main entry : 자바는 실행할 클래스의 main 메서드(함수)를 찾는다.
+		System.out.println("Hello World");
+//		콘솔화면에 문자열을 출력
+		System.out.println("git test");
+	}
+}
